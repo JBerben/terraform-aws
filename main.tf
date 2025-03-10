c4f7eae7f7763d6ad57fe9375087e82c6335fbab
JBerben <joshberben.work@gmail.com>
2025-03-10 16:56:45 +1100

    test

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
 #Define variables for AWS access key, secret key, and region
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
 
 resource "aws_iam_role" "hello_lambda_exec" {
   name = "hello-lambda"
