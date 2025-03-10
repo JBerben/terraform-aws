0a5ea0a6a7f3bd317d11c7a3897bbc0ff3bbf82f
JBerben <joshberben.work@gmail.com>
2025-03-10 14:39:24 +1100

    test

--- lambdas/s3-trigger/function.js
+++ lambdas/s3-trigger/function.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-    console.log("S3 Event Received:", JSON.stringify(event, null, 2));
+    console.log("Recieved S3 Event:", JSON.stringify(event, null, 2));
     const s3Record = event.Records[0];
     const bucketName = s3Record.s3.bucket.name;
     const objectKey = s3Record.s3.object.key;
