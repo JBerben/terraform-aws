7a369bbf364d8b43e99ec14c438a92615f5b41f1
JBerben <joshberben.work@gmail.com>
2025-03-11 14:09:50 +1100

    test

--- main.tf
+++ main.tf
@@ -30,6 +30,27 @@ terraform {
     required_version = "~> 1.0"
 }
 
+# Bucket for storing the hello-world lambda
+resource "random_pet" "lambda_bucket_name" {
+  prefix = "lambda"
+  length = 2
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket        = random_pet.lambda_bucket_name.id
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "lambda_bucket" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# hello-world lambda
 resource "aws_iam_role" "hello_lambda_exec" {
   name = "hello-lambda-ci"
 
@@ -90,21 +111,211 @@ resource "aws_s3_object" "lambda_hello" {
   etag = filemd5(data.archive_file.lambda_hello.output_path)
 }
 
-resource "random_pet" "lambda_bucket_name" {
-  prefix = "lambda"
+# Setting up the API Gateway
+resource "aws_apigatewayv2_api" "main" {
+  name          = "main-ci"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "dev" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  name        = "dev-ci"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.main_api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
+resource "aws_cloudwatch_log_group" "main_api_gw" {
+  name = "/aws/api-gw/${aws_apigatewayv2_api.main.name}"
+
+  retention_in_days = 14
+}
+
+# Link hello-world lambda to an API endpoint 
+resource "aws_apigatewayv2_integration" "lambda_hello" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  integration_uri    = aws_lambda_function.hello.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "get_hello" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_hello.id}"
+}
+
+resource "aws_apigatewayv2_route" "post_hello" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  route_key = "POST /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_hello.id}"
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
+}
+
+output "hello_base_url" {
+  value = aws_apigatewayv2_stage.dev.invoke_url
+}
+
+resource "random_pet" "test_bucket_name" {
+  prefix = "test"
   length = 2
 }
 
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket        = random_pet.lambda_bucket_name.id
+# Create a bucket 
+resource "aws_s3_bucket" "test" {
+  bucket        = random_pet.test_bucket_name.id
   force_destroy = true
 }
 
-resource "aws_s3_bucket_public_access_block" "lambda_bucket" {
-  bucket = aws_s3_bucket.lambda_bucket.id
+resource "aws_s3_bucket_public_access_block" "test" {
+  bucket = aws_s3_bucket.test.id
 
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_object" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  key     = "hello.json"
+  content = jsonencode({ name = "S3" })
+}
+
+output "test_s3_bucket" {
+  value = random_pet.test_bucket_name.id
+}
+
+# Upload S3 event trigger lambda to "test" bucket
+resource "aws_iam_role" "s3_lambda_exec" {
+  name = "s3-lambda-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "s3_lambda_policy" {
+  role       = aws_iam_role.s3_lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy" "test_s3_bucket_access" {
+  name        = "S3BucketAccess"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:ObjectCreated",
+          "s3:ListBucket"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.test.id}/*"
+      },
+    ]
+  })
+}
+
+# Give policy s3 bucket access
+resource "aws_iam_role_policy_attachment" "s3_lambda_test_s3_bucket_access" {
+  role       = aws_iam_role.s3_lambda_exec.name
+  policy_arn = aws_iam_policy.test_s3_bucket_access.arn
+}
+
+resource "aws_lambda_function" "s3" {
+  function_name = "s3-trigger"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_s3.key
+
+  runtime = "nodejs16.x"
+  handler = "function.handler"
+
+  source_code_hash = data.archive_file.lambda_s3.output_base64sha256
+
+  role = aws_iam_role.s3_lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "s3" {
+  name = "/aws/lambda/${aws_lambda_function.s3.function_name}"
+
+  retention_in_days = 14
+}
+
+data "archive_file" "lambda_s3" {
+  type = "zip"
+
+  source_dir  = "${path.module}/lambdas/s3-trigger"
+  output_path = "${path.module}/s3.zip"
+}
+
+resource "aws_s3_object" "lambda_s3" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "s3.zip"
+  source = data.archive_file.lambda_s3.output_path
+  
+  source_hash = filemd5(data.archive_file.lambda_s3.output_path)
+}
+
+# S3 Event Notification to Trigger Lambda
+resource "aws_s3_bucket_notification" "lambda_trigger" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.s3.arn
+    events             = ["s3:ObjectCreated:*"]
+  }
+}
+
+# Allow S3 to invoke Lambda
+resource "aws_lambda_permission" "s3_invoke_lambda" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.lambda_bucket.arn
 }
